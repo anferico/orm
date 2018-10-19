7170eb4106e860ab34e5e107ac326257a19cb6ad
anfri <f.cariaggi4@gmail.com>
2018-10-19 23:51:37 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,16 +7,6 @@ namespace AnnotationsProject
 	{
 		public static void Main(string[] args)
 		{
-			//string fileContent = File.ReadAllText("annotated_interfaces");
-			//var lexan = new LexicalAnalyzer(fileContent);
-
-			//Token token;
-			//do
-			//{
-			//	token = lexan.GetNextToken();
-			//	Console.WriteLine(token);
-			//} while (token != Token.ENDOFINPUT);
-
 			var parser = new Parser();
 			var tableAnnotations = parser.Parse("annotated_interfaces");
 
